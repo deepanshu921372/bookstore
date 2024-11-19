560b6c8d35c001dace70d1f3e85b6c95f9b0218b
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2024-11-19 22:05:12 +0530

    viewBookDetails fixed

--- frontend/src/components/ViewBookDetails/ViewBookDetails.jsx
+++ frontend/src/components/ViewBookDetails/ViewBookDetails.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Loader from "../Loader/Loader";
 import { GrLanguage } from "react-icons/gr";
 import { useParams } from "react-router-dom";
+import { FaHeart, FaShoppingCart } from "react-icons/fa";
 
 const ViewBookDetails = () => {
   const { id } = useParams();
@@ -22,8 +23,23 @@ const ViewBookDetails = () => {
     <>
       {data && (
         <div className="px-4 md:px-12 py-8 bg-zinc-900 flex gap-8 md:flex-row flex-col">
-          <div className="bg-zinc-800 rounded p-4 h-[60vh] lg:h-[88vh] w-full lg:w-3/6 flex items-center justify-center">
-            <img src={data?.url} alt="url" className="h-[50vh] lg:h-[70vh] rounded" />
+          <div className="w-full lg:w-3/6">
+            {" "}
+            <div className="rounded flex justify-around gap-8 p-12 bg-zinc-800">
+            <img
+              src={data?.url}
+              alt="url"
+              className="h-[50vh] lg:h-[70vh] rounded"
+            />
+            <div className="flex md:flex-col">
+              <button className="bg-white rounded-full text-red-500 text-3xl p-2">
+                <FaHeart />
+              </button>
+              <button className="bg-white rounded-full text-3xl text-blue-500 p-2 mt-4">
+                <FaShoppingCart />
+              </button>
+            </div>
+            </div>
           </div>
           <div className="p-4 w-full lg:w-3/6">
             <h1 className="text-4xl text-zinc-300 font-semibold">
